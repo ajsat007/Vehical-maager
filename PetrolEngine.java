7427a31bc5cfc75c8a2a7244b4b591644f80267e
Ajinkya Satkar <ajinkyasatkar007@gmail.com>
2023-03-06 20:38:23 +0530

    stop method added in engine classes

---
+++ PetrolEngine.java
@@ -0,0 +1,13 @@
+public class PetrolEngine {
+    public static void main(String[] args) {
+        PetrolEngine pE =new PetrolEngine();
+        pE.start();
+    }
+
+    public void start() {
+        System.out.println("Start");
+    }
+
+    
+    
+}
--- vehicle.java
+++ vehicle.java
@@ -1,5 +1,12 @@
 public class vehicle{
+   
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+          PetrolEngine pE= new PetrolEngine();
+     pE.start();
+
+     DieselEngine dE =new DieselEngine();
+     dE.start(); 
     }
 }
